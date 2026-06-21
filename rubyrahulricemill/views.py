a810569bd807d2fc5ab84f292329b70f0aa4c486
mohit <mohitgiri1103@gmail.com>
2026-06-22 01:35:43 +0530

    Final commit

---
+++ rubyrahulricemill/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def aboutus(request):
+    return render(request,'aboutus.html')
+
+def contactus(request):
+    return render(request,'contactus.html')
+
+def product(request):
+    return render(request,'product.html')
